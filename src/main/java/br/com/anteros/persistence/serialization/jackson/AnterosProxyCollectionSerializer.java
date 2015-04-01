f3a49e3f70c46514567040973de4a12c98b49c38
Edson Martins <edsonmartins2005@gmail.com>
2015-04-01 10:58:32 -0300

    Ajustes para usar Anteros-Persistence-Core.

--- src/main/java/br/com/anteros/persistence/serialization/jackson/AnterosProxyCollectionSerializer.java
+++ src/main/java/br/com/anteros/persistence/serialization/jackson/AnterosProxyCollectionSerializer.java
@@ -1,20 +1,27 @@
+/*******************************************************************************
+ * Copyright 2012 Anteros Tecnologia
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package br.com.anteros.persistence.serialization.jackson;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationHandler;
 
-import javassist.util.proxy.MethodHandler;
-import javassist.util.proxy.ProxyObject;
 import br.com.anteros.persistence.metadata.annotation.Fetch;
 import br.com.anteros.persistence.metadata.annotation.type.FetchMode;
 import br.com.anteros.persistence.metadata.annotation.type.FetchType;
-import br.com.anteros.persistence.proxy.AnterosProxyLob;
 import br.com.anteros.persistence.proxy.AnterosProxyObject;
-import br.com.anteros.persistence.proxy.JavassistLazyLoadInterceptor;
 import br.com.anteros.persistence.proxy.collection.AnterosPersistentCollection;
-import br.com.anteros.persistence.proxy.lob.BlobLazyLoadProxy;
-import br.com.anteros.persistence.proxy.lob.ClobLazyLoadProxy;
-import br.com.anteros.persistence.proxy.lob.NClobLazyLoadProxy;
 import br.com.anteros.persistence.serialization.jackson.AnterosPersistenceJacksonModule.Feature;
 import br.com.anteros.persistence.session.SQLSessionFactory;
 
--- src/main/java/br/com/anteros/persistence/serialization/jackson/AnterosProxySerializer.java
+++ src/main/java/br/com/anteros/persistence/serialization/jackson/AnterosProxySerializer.java
@@ -1,12 +1,23 @@
+/*******************************************************************************
+ * Copyright 2012 Anteros Tecnologia
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package br.com.anteros.persistence.serialization.jackson;
 
 import java.io.IOException;
 
-import javassist.util.proxy.MethodHandler;
-import javassist.util.proxy.ProxyObject;
 import br.com.anteros.persistence.proxy.AnterosProxyObject;
-import br.com.anteros.persistence.proxy.JavassistLazyLoadInterceptor;
-import br.com.anteros.persistence.serialization.jackson.AnterosPersistenceJacksonModule.Feature;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
